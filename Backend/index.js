0d528c59faf3746b35b5581a417f5fc7fed86f38
Pratham286 <22je0717@iitism.ac.in>
2026-02-15 06:22:30 +0530

    Added cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
 import { connectDB } from "./src/config/db.js";
 import app from "./src/app.js";
+import cors from "cors";
 
 dotenv.config({
   path: ".env",
 });
+
+
+app.use(cors({
+  origin: "https://one-geo-assignment.onrender.com/",
+  credentials: true
+}));
 
 connectDB();
 
